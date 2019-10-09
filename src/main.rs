6fc3e1ede2e50d74a8c14c88b71bf6f6a5e8e294
Aleksey Kolmogorov <alexv.kolmogorov@gmail.com>
2019-10-10 08:35:39 +1000

    added gcf funct

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
 fn main() {
     println!("Hello, world!");
     println!("123");
+    let num = gcd(0,7);
+    println!("num = {}", num);
 }
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
+
